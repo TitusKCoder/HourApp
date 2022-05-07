c264a3174f02baa6daf1b5b714869f4a6d04b144
Genevie Giko <gikogenevie@yahoo.com>
2022-05-07 08:48:09 -0400

    Client Side

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import MainImg from "../../Assets/mainPic.png";
+import Projects from "../Projects/Projects";
+import SocialMedia from "../SocialMedia";
+import TypeWriter from "./TypeWriter";
+
+function Home() {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Container className="home-content">
+          <Row>
+            <Col md={6} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Welcome To HOUR!{" "}
+              </h1>
+              <div className="heading-type">
+                <TypeWriter />
+              </div>
+            </Col>
+
+            <Col md={5}>
+              <img src={MainImg} className="profile-pic" alt="avatar" />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Container fluid className="home-about-section" id="about">
+        <Container>
+          <Row>
+            <Col md={12} className="home-about-social">
+              <h1>Grow, Learn and Connect</h1>
+              <SocialMedia />
+              <Projects />
+
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </section>
+  );
+}
+
+export default Home;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css'
-import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
 
 ReactDOM.render(
-<React.StrictMode>
+  <React.StrictMode>
     <App />
-</React.StrictMode>,
-document.getElementById('root'));
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
