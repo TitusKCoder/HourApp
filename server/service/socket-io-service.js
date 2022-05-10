16fc57a619075b0cbacf7fe31fb7cbdf5abf8e6a
Titus Knox <knoxtitus@gmail.com>
2022-05-10 18:15:16 -0400

    test tatk

--- server/service/socket-io-service.js
+++ server/service/socket-io-service.js
@@ -1,33 +1,33 @@
-const server = require('socket.io');
-import cors from 'cors'; 
+// const server = require('socket.io');
+// import cors from 'cors'; 
 
-app.use(cors());
+// app.use(cors());
 
-function socketService () {
+// function socketService () {
 
-const io = new Server(server, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"],
-    },
-  });
+// const io = new Server(server, {
+//     cors: {
+//       origin: "http://localhost:3000",
+//       methods: ["GET", "POST"],
+//     },
+//   });
 
-  io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
+//   io.on("connection", (socket) => {
+//     console.log(`User Connected: ${socket.id}`);
   
-    socket.on("join_room", (data) => {
-      socket.join(data);
-      console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    });
+//     socket.on("join_room", (data) => {
+//       socket.join(data);
+//       console.log(`User with ID: ${socket.id} joined room: ${data}`);
+//     });
   
-    socket.on("send_message", (data) => {
-      socket.to(data.room).emit("receive_message", data);
-    });
+//     socket.on("send_message", (data) => {
+//       socket.to(data.room).emit("receive_message", data);
+//     });
   
-    socket.on("disconnect", () => {
-      console.log("User Disconnected", socket.id);
-    });
-  });
-}
+//     socket.on("disconnect", () => {
+//       console.log("User Disconnected", socket.id);
+//     });
+//   });
+// }
 
-export default socketService;
+// export default socketService;
