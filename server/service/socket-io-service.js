d34f9da98838363751c821636cd64a83a584b195
Genevie Giko <gikogenevie@yahoo.com>
2022-05-11 07:41:36 -0400

    Created routes

--- server/service/socket-io-service.js
+++ server/service/socket-io-service.js
@@ -1,87 +1,84 @@
 require('dotenv').config();
-const path = require('path');
-const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
-const PORT = process.env.PORT || 3001;
-
-const app = express();
-import cors from 'cors'; 
-
-app.use(cors());
-
-const server = http.createServer(app);
-const io = socketio(server);
-
-if (process.env.PROD) {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build/index.html'));
-    });
-} else {
-    app.use(express.static(path.join(__dirname, '/')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'index.html'));
-    });
-}
-
-  io.on('connect', (socket) => {
-    console.log('Welcome!');
-
-    socket.on('join', ({ name, room }, callback) => {
-        const { error, user } = addUser({ id: socket.id, name, room });
-
-        if (error) return callback(error);
-
-        socket.emit('message', {
-            user: 'admin',
-            text: `${user.name}, Welcome to the room ${user.room}!`,
-        });
-
-        socket.broadcast.to(user.room).emit('message', {
-            user: 'admin',
-            text: `${user.name} has Joined.`,
-        });
-
-        socket.join(user.room);
-
-        io.to(user.room).emit('roomData', {
-            room: user.room,
-            users: getUsersInRoom(user.room),
-        });
-
-        callback();
-    });
-
-    socket.on('sendMessage', (message, callback) => {
-        const user = getUser(socket.id);
-
-        io.to(user.room).emit('message', { user: user.name, text: message });
-
-        callback();
-    });
-
-    socket.on('disconnect', () => {
-        const user = removeUser(socket.id);
-
-        if (user) {
-            io.to(user.room).emit('message', {
-                user: 'admin',
-                text: `${user.name} has left.`,
-            });
-            io.to(user.room).emit('roomData', {
-                room: user.room,
-                users: getUsersInRoom(user.room),
-            });
-        }
-    });
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 
-server.listen(PORT, () => {
-    console.log(`**************************************`);
-    console.log(`Server is running on port: ${PORT}`);
-    console.log(`URL address: http://localhost:${PORT}`);
-    console.log(`**************************************`);
+mongoose.connection.on("error", (err) => {
+  console.log("Mongoose Connection ERROR: " + err.message);
+});
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB Connected!");
+});
+
+//Bring in the models
+require("./models/User");
+require("./models/Chatroom");
+require("./models/Message");
+
+const app = require("./app");
+
+const server = app.listen(8000, () => {
+  console.log("Server listening on port 8000");
+});
+
+const io = require("socket.io")(server, {
+  allowEIO3: true,
+  cors: {
+    origin: true,
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+});
+
+const jwt = require("jwt-then");
+
+const Message = mongoose.model("Message");
+const User = mongoose.model("User");
+
+io.use(async (socket, next) => {
+  try {
+    const token = socket.handshake.query.token;
+    const payload = await jwt.verify(token, process.env.SECRET);
+    socket.userId = payload.id;
+    next();
+  } catch (err) {}
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected: " + socket.userId);
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected: " + socket.userId);
+  });
+
+  socket.on("joinRoom", ({ chatroomId }) => {
+    socket.join(chatroomId);
+    console.log("A user joined chatroom: " + chatroomId);
+  });
+
+  socket.on("leaveRoom", ({ chatroomId }) => {
+    socket.leave(chatroomId);
+    console.log("A user left chatroom: " + chatroomId);
+  });
+
+  socket.on("chatroomMessage", async ({ chatroomId, message }) => {
+    if (message.trim().length > 0) {
+      const user = await User.findOne({ _id: socket.userId });
+      const newMessage = new Message({
+        chatroom: chatroomId,
+        user: socket.userId,
+        message,
+      });
+      io.to(chatroomId).emit("newMessage", {
+        message,
+        name: user.name,
+        userId: socket.userId,
+      });
+      await newMessage.save();
+    }
+  });
 });
